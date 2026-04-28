18c1eea4cd3754942da40886d39b7476f613e3d3
timgrindall <tim.grindall@gmail.com>
2026-04-27 20:49:02 -0700

    refactor into seperate files

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+// const middleware = require('./utils/middleware')
+
 const app = express()
 
+if (!config.MONGODB_URI) {
+  console.log('No MONGO_URI ENV variable!')
+  process.exit(1)
+}
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
 app.use(express.json())
-
-const blogsRouter = require('./controllers/blogs')
-
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- models/blog.js
+++ models/blog.js
@@ -1,22 +1,4 @@
-require('dotenv').config()
-// console.log("ENV:", process.env.MONGODB_URI)
-
 const mongoose = require('mongoose')
-
-if (!process.env.MONGODB_URI) {
-  console.log('No MONGO_URI ENV variable!')
-  process.exit(1)
-}
-
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl, { family: 4 })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const blogSchema = mongoose.Schema({
   title: String,

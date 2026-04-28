b8f67062acbf2eefcf0b5aa27db656c08b9d5f55
timgrindall <tim.grindall@gmail.com>
2026-04-28 16:40:11 -0700

    fully structured

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
-// const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
@@ -13,13 +14,19 @@ if (!config.MONGODB_URI) {
 
 mongoose.connect(config.MONGODB_URI, { family: 4 })
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
